3ae262d63e1142db4f1e6a21ea0ff60a001a849c
Boss <cure1218@gmail.com>
2024-07-28 17:34:28 +0000

    boss

--- core.go
+++ core.go
@@ -15,6 +15,10 @@ func (id ID) IsZero() bool {
 	return snowflake.ID(id) == 0
 }
 
+func (id ID) MarshalJSON() ([]byte, error) {
+	return json.Marshal(id.String())
+}
+
 func (id *ID) UnmarshalJSON(data []byte) error {
 	var str string
 	if err := json.Unmarshal(data, &str); err != nil {
